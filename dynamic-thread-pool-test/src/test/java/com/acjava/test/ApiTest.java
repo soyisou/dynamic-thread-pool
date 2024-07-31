51de330d5864b85640c894834f7104f6ad37822e
Lou Jimeng <ljm878665865@gmail.com>
2024-08-01 01:19:06 +0800

    feat: 更新测试代码

--- dynamic-thread-pool-test/src/test/java/com/acjava/test/ApiTest.java
+++ dynamic-thread-pool-test/src/test/java/com/acjava/test/ApiTest.java
@@ -1,9 +1,15 @@
 package com.acjava.test;
 
+import com.acjava.middleware.dynamic.thread.pool.sdk.domain.model.entity.ThreadPoolConfigEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.redisson.api.RTopic;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @Author loujm  acjava.com @铭轩
@@ -14,4 +20,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiTest {
+
+    @Resource
+    private RTopic dynamicThreadPoolRedisTopic;
+
+    @Test
+    public void test_dynamicThreadPoolRedisTopic() throws InterruptedException {
+        ThreadPoolConfigEntity threadPoolConfigEntity = new ThreadPoolConfigEntity("dynamic-thread-pool-test-app", "threadPoolExecutor01");
+        threadPoolConfigEntity.setPoolSize(100);
+        threadPoolConfigEntity.setMaximumPoolSize(100);
+        dynamicThreadPoolRedisTopic.publish(threadPoolConfigEntity);
+
+        new CountDownLatch(1).await();
+    }
 }

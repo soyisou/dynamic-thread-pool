7fa0935d69bb150ec4806560dbec9aff4d815a79
Lou Jimeng <ljm878665865@gmail.com>
2024-08-01 01:15:01 +0800

    feat: 更新动态线程池测试模块代码

---
+++ dynamic-thread-pool-test/src/test/java/com/acjava/test/ApiTest.java
@@ -0,0 +1,17 @@
+package com.acjava.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author loujm  acjava.com @铭轩
+ * @Description 测试类
+ * @Date 2024/7/31 下午9:50
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiTest {
+}
